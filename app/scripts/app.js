5dd3bb3ef67568c4b27bc0ed5ccac3066573f5ac
Karsten Westra <karstenwestra@gmail.com>
2015-06-29 00:18:25 +0200

    Stripped polymer app with first attempt at collapsing slant-box element.

---
+++ app/scripts/app.js
@@ -0,0 +1,11 @@
+(function(/*document*/) {
+  'use strict';
+
+  // See https://github.com/Polymer/polymer/issues/1381
+  window.addEventListener('WebComponentsReady', function() {
+    // imports are loaded and elements have been registered
+    console.log('Web components are ready!');
+    // document.querySelector('#app');
+  });
+
+})(/*document*/);
